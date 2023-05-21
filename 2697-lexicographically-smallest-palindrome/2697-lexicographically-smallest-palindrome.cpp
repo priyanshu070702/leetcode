ffed90d0959237e40ae4d16b220e06f8a61a77d1
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-05-21 16:45:52 +0530

    Time: 38 ms (83.33%), Space: 15 MB (66.67%) - LeetHub

---
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int i = 0, j = s.length()-1;
+        while(i<j){
+            if(s[i] != s[j]){
+                s[i] = s[j] = min(s[i],s[j]);
+            }
+            i++;j--;
+        }
+        return s;
+    }
+};
