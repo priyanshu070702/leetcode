5c895170e2c4b500d6b756fe9b95723d0ea6e438
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-05-31 00:23:36 +0530

    Time: 1205 ms (11.04%), Space: 8.1 MB (51.46%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool func(vector<vector<char>>&b, string w, int i,int j, int ind){
+        if(ind==w.size())return true;
+        if(i<0||j<0||i>=b.size()||j>=b[0].size()||b[i][j]!=w[ind])return false;
+        char temp=b[i][j];
+        b[i][j]='0';
+        bool x = func(b,w,i+1,j,ind+1);
+        bool y = func(b,w,i-1,j,ind+1);
+        bool z = func(b,w,i,j+1,ind+1);
+        bool n = func(b,w,i,j-1,ind+1);
+        b[i][j]=temp;
+        return(x||y||z||n);
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size();
+        int m = board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==word[0]&&func(board,word,i,j,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
