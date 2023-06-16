152afa3c95609eee937f94eaa95b5a4c040e3a06
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-06-16 23:19:59 +0530

    Time: 40 ms (19.51%), Space: 23.2 MB (97.56%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,20 +11,35 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL) return NULL;
-        if(root->val == p->val || root->val == q->val)return root;
-
-        if(root->val > p->val && root->val < q->val)return root;
-        else if(root->val > q->val && root->val < p->val)return root;
-
-        if(root->val > p->val && root->val > q->val){
-            return lowestCommonAncestor(root->left,p,q);
+        int sp=p->val;
+        int sq=q->val;
+        while(root!=NULL){
+            if(sp>root->val && sq>root->val){
+                root=root->right;
+            }
+            else if(sp<root->val && sq<root->val){
+                root=root->left;
+            }
+            else{
+                return root;
+            }
         }
-        else if(root->val < p->val && root->val < q->val){
-            return lowestCommonAncestor(root->right,p,q);
-        }
-
         return NULL;
+//         if(root==NULL) return NULL;
+//         if(root->val == p->val || root->val == q->val)return root;
+
+//         if(root->val > p->val && root->val < q->val)return root;
+//         else if(root->val > q->val && root->val < p->val)return root;
+
+//         if(root->val > p->val && root->val > q->val){
+//             return lowestCommonAncestor(root->left,p,q);
+//         }
+//         else if(root->val < p->val && root->val < q->val){
+//             return lowestCommonAncestor(root->right,p,q);
+//         }
+
+//         return NULL;
+        
         // if(root->val==p->val)return root;
         // if(root->val==q->val)return root;
         // while(root!=NULL){
