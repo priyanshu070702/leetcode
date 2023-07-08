540f1c2fb903fb6b62973ced070e008cf6c1c5d2
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-07-08 10:23:32 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (5.55%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,27 +1,48 @@
 class Solution {
 public:
-        bool issafe(int i,int j , int m , int n  ){
-       if(i >= 0 && j >= 0 && i < m && j < n ) return true;
-       return false;
-   }
-    int solve(int m  , int n , int i , int j,vector<vector<int>>&dp){
-        // base case 
-        if(i == m - 1 && j == n - 1) return 1;
-       
-        if(dp[i][j] != -1) return dp[i][j];
-       
-        int ans = 0;
-        
-        // righ check 
-        if(issafe(i,j+1, m , n)) ans +=  solve(m,n,i,j+1,dp);
-        
-        // douwn check
-        if(issafe(i+1,j,m,n)) ans +=  solve(m,n,i+1,j,dp);
-        
-        return dp[i][j] = ans;
+    bool isSafe(int i, int j, int m,int n){
+        if(i>=0 && i<=m &&j>=0 && j<=n){
+            return true;
+        }
+        return false;
     }
+    int solveRec(int i, int j, int m, int n){
+        if(i==m && j==n){
+            return 1;
+        }
+        //down
+        int down=0;
+        if(isSafe(i+1,j,m,n)){
+            down+=solveRec(i+1,j,m,n);
+        }
+        //right;
+        int right=0;
+        if(isSafe(i,j+1,m,n)){
+            right=solveRec(i,j+1,m,n);
+        }
+        return right+down;
+    } 
+    
+    int solveMemo(int i, int j, int m, int n, vector<vector<int>>&dp){
+        if(i==m && j==n){
+            return 1;
+        }
+        if(dp[i][j]!=-1)return dp[i][j];
+        //down
+        int down=0;
+        if(isSafe(i+1,j,m,n)){
+            down+=solveMemo(i+1,j,m,n,dp);
+        }
+        //right;
+        int right=0;
+        if(isSafe(i,j+1,m,n)){
+            right=solveMemo(i,j+1,m,n,dp);
+        }
+        return dp[i][j]=right+down;
+    }
+    
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(m,n,0,0,dp);
+        return solveMemo(0,0,m-1,n-1,dp);
     }
 };
