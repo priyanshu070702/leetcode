3755e60949e48f61caceeea9c85ba7cae4f67896
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-07-08 10:43:01 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (21.05%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -41,8 +41,26 @@ public:
         return dp[i][j]=right+down;
     }
     
+    int solveTab(int m, int n){
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        dp[m-1][n-1]=1;
+        for(int j=0;j<n;j++){
+            dp[m-1][j]=1;
+        }
+        for(int i=0;i<m;i++){
+            dp[i][n-1]=1;
+        }
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                dp[i][j]=dp[i+1][j]+dp[i][j+1];
+            }
+        }
+        return dp[0][0];
+    }
+    
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solveMemo(0,0,m-1,n-1,dp);
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        // return solveMemo(0,0,m-1,n-1,dp);
+        return solveTab(m,n);
     }
 };
