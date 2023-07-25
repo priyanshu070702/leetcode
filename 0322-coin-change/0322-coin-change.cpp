01628a330d3269420d7a89d56683b0c9e6e7620b
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-07-25 22:32:23 +0530

    Time: 238 ms (7.97%), Space: 53.4 MB (6.61%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -13,11 +13,32 @@ public:
         int nottake=coinRec(coins,amount,idx+1,n,dp);
         return dp[idx][amount] = min(take,nottake);
     }
+    int solveTab(vector<int>coins, int amount){
+        int n=coins.size();
+        vector<vector<int>>dp(n+2,vector<int>(amount+2,1e9));
+        for(int idx=n-1;idx>=0;idx--){
+            dp[idx][0]=0;
+        }
+        for(int idx=n-1;idx>=0;idx--){
+            for(int j=1;j<=amount;j++){
+                int take=INT_MAX;
+                if(coins[idx]<=j){
+                    take=1+dp[idx][j-coins[idx]]; 
+                }
+                int nottake=dp[idx+1][j];
+                    
+                dp[idx][j] = min(take,nottake);
+                }
+            }
+        if(dp[0][amount]==1e9)return -1;
+        return dp[0][amount];
+         }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans = coinRec(coins, amount, 0, n, dp);
-        if(ans==1e9)return -1;
-        return ans;
+        // vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        // int ans = coinRec(coins, amount, 0, n, dp);
+        // if(ans==1e9)return -1;
+        // return ans;
+        return solveTab(coins,amount);
     }
 };
