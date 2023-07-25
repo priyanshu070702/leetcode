73bec63bcc8add4b8ad7c38e8fe2afa0d2ee505f
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-07-25 11:25:21 +0530

    Time: 120 ms (79.74%), Space: 59.7 MB (40.39%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        for(int i=0;i<arr.size();i++){
-            if(i==0 && arr[i+1]<arr[i])return i;
-            else if(i==arr.size()-1 && arr[i-1]<arr[i])return i;
-            else if(arr[i]>arr[i+1] && arr[i]>arr[i-1])return i;
+        // for(int i=0;i<arr.size();i++){
+        //     if(i==0 && arr[i+1]<arr[i])return i;
+        //     else if(i==arr.size()-1 && arr[i-1]<arr[i])return i;
+        //     else if(arr[i]>arr[i+1] && arr[i]>arr[i-1])return i;
+        // }
+        // return -1;
+        
+        //optimized approach
+        int s=1,e=arr.size()-1;
+        while(s<=e){
+            int mid=(s+e)/2;
+            if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1])return mid;
+            else if(arr[mid]<arr[mid+1])s=mid+1;
+            else e=mid-1;
         }
         return -1;
     }
