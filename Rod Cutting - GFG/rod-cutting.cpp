398f0eac1bcbdd8bf521a526a7e595ab65b1194b
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-07-31 01:08:45 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -25,9 +25,27 @@ class Solution{
         
         return dp[n][idx] = max(take,not_take);
     }
+    int solveTab(int price[], int n){
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+        for(int i=0;i<=n;i++){
+            for(int idx=n-1;idx>=0;idx--){
+                //take
+                int take=INT_MIN;
+                if(idx+1<=i){
+                    take=price[idx]+dp[i-(idx+1)][idx];
+                }
+                //not take
+                int not_take=dp[i][idx+1];
+                
+                dp[i][idx]=max(take,not_take);
+            }
+        }
+        return dp[n][0];
+    }
     int cutRod(int price[], int n) {
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solveMemo(price,n,0,n,dp);
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        // return solveMemo(price,n,0,n,dp);
+        return solveTab(price,n);
     }
 };
 
