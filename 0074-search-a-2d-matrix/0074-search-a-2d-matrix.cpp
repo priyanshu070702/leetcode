8cdd453e98d7096252af72563a9ae1c29bd4ff4c
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-08-07 21:43:38 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (90.26%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        //O(n*m)
+        // for(int i=0;i<matrix.size();i++){
+        //     for(int j=0;j<matrix[0].size();j++){
+        //         if(matrix[i][j]==target)return true;
+        //     }
+        // }
+        // return false;
+        
+        //O(log(n*m)) optimized;
         for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                if(matrix[i][j]==target)return true;
+            int start=0,end=matrix[i].size()-1;
+            while(start<=end){
+                int mid=(start+end)/2;
+                if(matrix[i][mid]==target)return true;
+                else if(matrix[i][mid]>target)end=mid-1;
+                else start=mid+1;
             }
         }
         return false;
