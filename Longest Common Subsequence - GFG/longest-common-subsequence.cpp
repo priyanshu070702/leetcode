019ba1b1c089e274278e8ab4186b026e3316f2db
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-08-10 12:42:48 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -10,58 +10,43 @@ class Solution
 {
     public:
     //Function to find the length of longest common subsequence in two strings.
-    int solveMemo(int n, int m, string s1, string s2, int i, int j, vector<vector<int>>&dp){
-        if(i==n||j==m)return 0;
+    int solveRec(string s1, string s2, int i, int j, vector<vector<int>>&dp){
+        if(i==s1.length() || j==s2.length()){
+            return 0;
+        }
         if(dp[i][j]!=-1)return dp[i][j];
         int len=0;
         if(s1[i]==s2[j]){
-            len=1+solveMemo(n,m,s1,s2,i+1,j+1,dp);
+            len=1+solveRec(s1,s2,i+1,j+1,dp);
         }
         else{
-            len=max(solveMemo(n,m,s1,s2,i+1,j,dp),solveMemo(n,m,s1,s2,i,j+1,dp));
+            len=max(solveRec(s1,s2,i,j+1,dp),solveRec(s1,s2,i+1,j,dp));
         }
         return dp[i][j]=len;
     }
-    int solveTab(int n, int m, string s1, string s2){
+    int solveRec(string s1, string s2){
+        int n=s1.length();
+        int m=s2.length();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
                 int len=0;
                 if(s1[i]==s2[j]){
                     len=1+dp[i+1][j+1];
-                } 
+                }
                 else{
-                    len=max(dp[i+1][j],dp[i][j+1]);
+                    len=max(dp[i][j+1],dp[i+1][j]);
                 }
                 dp[i][j]=len;
             }
         }
         return dp[0][0];
     }
-    int solveOP(int n, int m, string s1, string s2){
-        vector<int>curr(m+1,0);
-        vector<int>next(m+1,0);
-        for(int i=n-1;i>=0;i--){
-            for(int j=m-1;j>=0;j--){
-                int len=0;
-                if(s1[i]==s2[j]){
-                    len=1+next[j+1];
-                } 
-                else{
-                    len=max(next[j],curr[j+1]);
-                }
-                curr[j]=len;
-            }
-            next=curr;
-        }
-        return next[0];
-        
-    }
     int lcs(int n, int m, string s1, string s2)
     {
         // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        // return solveMemo(n,m,s1,s2,0,0,dp);
-        return solveOP(n,m,s1,s2);
+        // return solveRec(s1,s2,0,0,dp);
+        return solveRec(s1,s2);
     }
 };
 
