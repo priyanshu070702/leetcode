63c63cbea8344ee8a61921395733f45c76257a40
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-08-14 19:32:08 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (94.85%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        nums.insert(nums.begin(),INT_MIN);
-        nums.push_back(INT_MIN);
-        int n=nums.size();
-        for(int i=1;i<n-1;i++){
-            if(nums[i]>nums[i-1] && nums[i]>nums[i+1])return i-1;
+        //linear search
+        // nums.insert(nums.begin(),INT_MIN);
+        // nums.push_back(INT_MIN);
+        // int n=nums.size();
+        // for(int i=1;i<n-1;i++){
+        //     if(nums[i]>nums[i-1] && nums[i]>nums[i+1])return i-1;
+        // }
+        // return 0;
+        
+        //binary search
+        int s=0;
+        int e=nums.size()-1;
+        while(s<e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]<nums[mid+1]){
+                s=mid+1;
+            }
+            else{
+                e=mid;
+            }
         }
-        return 0;
+        return e;
     }
 };
