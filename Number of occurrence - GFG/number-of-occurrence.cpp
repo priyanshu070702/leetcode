215ddcc5b39f073d6759b405d1e76c07f1865962
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-08-20 22:38:02 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -11,11 +11,49 @@ public:
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
 	int count(int arr[], int n, int x) {
-	    int cnt=0;
-	    for(int i=0;i<n;i++){
-	        if(x==arr[i])cnt++;
-	    }
-	    return cnt;
+	    //O(n)
+	   // int cnt=0;
+	   // for(int i=0;i<n;i++){
+	   //     if(x==arr[i])cnt++;
+	   // }
+	   // return cnt;
+	   
+	   //O(logn)
+	   int low=0,high=n-1;
+	   int first_occurence=-1;
+	   while(low<=high){
+	       int mid=low+(high-low)/2;
+	       if(arr[mid]==x){
+	           first_occurence=mid;
+	           high=mid-1;
+	       }
+	       else if(arr[mid]<x){
+	           low=mid+1;
+	       }
+	       else{
+	           high=mid-1;
+	       }
+	   }
+	   
+	   if(first_occurence==-1)return 0;
+	   
+	   low=0,high=n-1;
+	   int last_occurence=0;
+	   while(low<=high){
+	       int mid=low+(high-low)/2;
+	       if(arr[mid]==x){
+	           last_occurence=mid;
+	           low=mid+1;
+	       }
+	       else if(arr[mid]<x){
+	           low=mid+1;
+	       }
+	       else{
+	           high=mid-1;
+	       }
+	   }
+	   int number_of_occurences=last_occurence-first_occurence+1;
+	   return number_of_occurences;
 	}
 };
 
