6b04f69e27ce0df23449698f317ab0b65512ddc2
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-08-25 15:14:17 +0530

    Time: 72 ms (70.53%), Space: 58.9 MB (73.63%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,14 +1,30 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int water = 0;
-    int i = 0, j = height.size() - 1;
-    while (i < j) {
-        int h = min(height[i], height[j]);
-        water = max(water, (j - i) * h);
-        if (height[i] <= h && i < j) i++;
-        if (height[j] <= h && i < j) j--;
-    }
-    return water;
+       //brute force approach
+        // int ans=INT_MIN;
+        // for(int i=0;i<height.size();i++){
+        //     for(int j=i+1;j<height.size();j++){
+        //         int mx=(j-i)*(min(height[i],height[j]));
+        //         ans=max(ans,mx);
+        //     }
+        // }
+        // return ans;
+        
+        //optimized approroach
+        int i=0;
+        int j=height.size()-1;
+        
+        int mx=INT_MIN;
+        
+        while(i<=j){
+            int min_height=min(height[i],height[j])*(j-i);
+            mx=max(mx,min_height);
+            if(height[i]<=height[j]){
+                i++;
+            }
+            else j--;
+        }
+        return mx;
     }
 };
