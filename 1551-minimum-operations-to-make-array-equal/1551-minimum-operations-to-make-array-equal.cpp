4aba9ceb39ff46cc4232b24d9e50c538652b6e6a
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-08-31 16:03:45 +0530

    Time: 2 ms (52.65%), Space: 5.9 MB (92.13%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     int minOperations(int n) {
-        vector<int>v(n);
-        for(int i=0;i<n;i++){
-            v[i]=(2*i)+1;
-        }
-        int i=0,j=n-1;
+        // vector<int>v(n);
+        // for(int i=0;i<n;i++){
+        //     v[i]=(2*i)+1;
+        // }
+        // int i=0,j=n-1;
         int sum=0;
-        while(i<=j){
-            sum+=(v[j]-v[i])/2;
-            i++;j--;
+        // while(i<=j){
+        //     sum+=(v[j]-v[i])/2;
+        //     i++;j--;
+        // }
+        // return sum;
+        if(n%2!=0){
+            for(int i=1;i<=n;i++){
+                if(i%2==0)sum+=i;
+            }
+        }
+        else{
+            for(int i=1;i<=n;i++){
+                if(i%2!=0)sum+=i;
+            }
         }
         return sum;
     }
