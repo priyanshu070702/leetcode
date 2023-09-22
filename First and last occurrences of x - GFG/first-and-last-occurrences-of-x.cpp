d54dc9668e1982b2d0c3a533e2b1ea52cc10b4e9
PRIYANSHU DHAPODKAR <priyanshu.dhapodkar2020@vitbhopal.ac.in>
2023-09-22 11:36:01 +0530

    Added solution - LeetHub

--- First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
@@ -8,22 +8,81 @@ class Solution
     public:
     vector<int> find(int arr[], int n , int x )
     {
-        vector<int>v;
-        for(int i=0;i<n;i++){
-            if(arr[i]==x){
-                v.push_back(i);
-                break;
+        //bruteforce approach
+        // vector<int>v;
+        // for(int i=0;i<n;i++){
+        //     if(arr[i]==x){
+        //         v.push_back(i);
+        //         break;
+        //     }
+        //     if(i==n-1)v.push_back(-1);
+        // }
+        // for(int i=n-1;i>=0;i--){
+        //     if(arr[i]==x){
+        //         v.push_back(i);
+        //         break;
+        //     }
+        //     if(i==0)v.push_back(-1);
+        // }
+        // return v;
+        vector<int>v(2,-1);
+        int low=0,high=n-1;
+        
+        int ans_first=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==x){
+                ans_first=mid;
+                high=mid-1;
             }
-            if(i==n-1)v.push_back(-1);
-        }
-        for(int i=n-1;i>=0;i--){
-            if(arr[i]==x){
-                v.push_back(i);
-                break;
+            else if(arr[mid]>x){
+                high=mid-1;
             }
-            if(i==0)v.push_back(-1);
+            else if(arr[mid]<x){
+                low=mid+1;
+            }
         }
+        v[0]=ans_first;
+        
+        int ans_last=-1;
+        low=0,high=n-1;
+        
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==x){
+                ans_last=mid;
+                low=mid+1;
+            }
+            else if(arr[mid]>x){
+                high=mid-1;
+            }
+            else if(arr[mid]<x){
+                low=mid+1;
+            }
+        }
+        v[1]=ans_last;
+        
+        
         return v;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 };
 
